22efb116ed7966813ff92455074c7759f2b84171
Chris Loew <cloew123@gmail.com>
2014-09-29 12:05:09 -0600

    Primitive Parameter now can have an optional value

--- src/kao_factory/Parameter/Test/primitive_parameter_test.py
+++ src/kao_factory/Parameter/Test/primitive_parameter_test.py
@@ -8,17 +8,25 @@ class getValue(unittest.TestCase):
     def  setUp(self):
         """ Build the Parameter for the test """
         self.field = "name"
+        self.otherField = "Other Field"
         self.expectedValue = "Some Value"
         self.data = {self.field:self.expectedValue}
+        
         self.parameter = PrimitiveParameter(self.field)
+        self.optionalParameter = PrimitiveParameter(self.otherField, optional=True, default=self.expectedValue)
         
     def retrieved(self):
         """ Test that the value retrieved from the parameter is correct """
         value = self.parameter.getValue(self.data)
         self.assertEqual(value, self.expectedValue, "The Value should match the expected value")
+        
+    def optionalValue(self):
+        """ Test that the value retrieved from the parameter is correct """
+        value = self.optionalParameter.getValue(self.data)
+        self.assertEqual(value, self.expectedValue, "The Value should match the expected value")
 
 # Collect all test cases in this class
-testcasesGetValue = ["retrieved"]
+testcasesGetValue = ["retrieved", "optionalValue"]
 suiteGetValue = unittest.TestSuite(map(getValue, testcasesGetValue))
 
 ##########################################################
--- src/kao_factory/Parameter/primitive_parameter.py
+++ src/kao_factory/Parameter/primitive_parameter.py
@@ -2,10 +2,17 @@
 class PrimitiveParameter:
     """ Represents a parameter that is based on a simple Primitive Value """
     
-    def __init__(self, field):
+    def __init__(self, field, optional=False, default=None):
         """ Initialize the Primitive Parameter with the field it should load the value from """
         self.field = field
+        self.optional = optional
+        self.defaultValue = default
         
     def getValue(self, data):
         """ Get the Primitive value from the data """
-        return data[self.field]
+        if self.field in data:
+            return data[self.field]
+        elif self.optional:
+            return self.defaultValue
+        else:
+            pass # Probably want to throw an exception
