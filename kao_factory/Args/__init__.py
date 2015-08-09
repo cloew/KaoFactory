eb08eac3333cf153f65255459864a14edb049238
Chris Loew <cloew123@gmail.com>
2015-08-09 14:14:57 -0600

    Removed the old Parameter classes

--- kao_factory/Args/__init__.py
+++ kao_factory/Args/__init__.py
@@ -1,3 +1,1 @@
 from .field_arg import FieldArg
-# from .complex_parameter import ComplexParameter
-# from .primitive_parameter import PrimitiveParameter
--- kao_factory/Args/primitive_parameter.py
+++
@@ -1,8 +0,0 @@
-from .parameter import Parameter
-
-class PrimitiveParameter(Parameter):
-    """ Represents a parameter that is based on a simple Primitive Value """
-        
-    def __getvalue__(self, data):
-        """ Get the Primitive value from the data """
-        return data[self.field]
